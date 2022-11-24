c6295662bffde321996fcb2b67a5e505057655c1
bego <begomontes7@gmail.com>
2022-11-24 19:32:09 +0100

    Added basic functionality to display the books inside the shelfs

--- starter/src/SearchBooks.js
+++ starter/src/SearchBooks.js
@@ -1,17 +1,18 @@
 import {useEffect, useState} from "react";
 import * as BooksAPI from "./BooksAPI";
 import {Link} from "react-router-dom";
+import ListBooks from "./ListBooks";
 
 
 
 const SearchBooks = ()=>{
 
     const [books, setBooks] = useState([]);
-    
-    useEffect(()=>{
 
+    useEffect(()=>{
         const getBooks = async ()=>{
           const res = await BooksAPI.getAll();
+          console.log(res);
           setBooks(res);
         }
     
@@ -20,20 +21,24 @@ const SearchBooks = ()=>{
     }, []);
 
     return (
-        <div className="search-books">
-            <div className="search-books-bar">
-                <Link className="close-search" to="/">Close</Link>
-                <div className="search-books-input-wrapper">
-                    <input
-                    type="text"
-                    placeholder="Search by title, author, or ISBN"
-                    />
+        <div>
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link className="close-search" to="/">Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <input
+                        type="text"
+                        placeholder="Search by title, author, or ISBN"
+                        />
+                    </div>
+                    </div>
+                    <div className="search-books-results">
+                    <ol className="books-grid"></ol>
                 </div>
-                </div>
-                <div className="search-books-results">
-                <ol className="books-grid"></ol>
             </div>
+            <ListBooks books ={books}/>
         </div>
+        
     )
 
 }
