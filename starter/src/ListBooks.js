20f0c852b94c19d125c8f5c803cfbc989ba8bb8a
bego <begomontes7@gmail.com>
2022-11-25 19:08:42 +0100

    Finished problem with books that dont have an image

--- starter/src/ListBooks.js
+++ starter/src/ListBooks.js
@@ -4,46 +4,49 @@ const ListBooks = ({ books }) => {
     return (
         <ol className="books-grid">
             {
-                books !== [] && (
-                    books.map((book) => (
-                        <li key={book.id}>
-                            <div className="book">
-                                <div className="book-top">
-                                    <div
-                                        className="book-cover"
-                                        style={{
-                                            width: 128,
-                                            height: 193,
-                                            backgroundImage:`url("${book.imageLinks.smallThumbnail}")`
-                                        }}
-                                    ></div>
-                                    <div className="book-shelf-changer">
-                                        <select>
-                                            <option value="none" disabled>
-                                                Move to...
-                                            </option>
-                                            <option value="currentlyReading">
-                                                Currently Reading
-                                            </option>
-                                            <option value="wantToRead">Want to Read</option>
-                                            <option value="read">Read</option>
-                                            <option value="none">None</option>
-                                        </select>
+                books.filter((book)=>book.imageLinks)
+                    .map((book) => (
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                        <div
+                                            className="book-cover"
+                                            style={{
+                                                width: 128,
+                                                height: 193,
+                                                backgroundImage:`url("${book.imageLinks.smallThumbnail}")`
+                                            }}
+                                        ></div>
+                                        <div className="book-shelf-changer">
+                                            <select>
+                                                <option value="none" disabled>
+                                                    Move to...
+                                                </option>
+                                                <option value="currentlyReading">
+                                                    Currently Reading
+                                                </option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="book-title">{book.title}</div>
-                                {
-                                    book.authors ? <div className="book-authors">
+                                    <div className="book-title">{book.title}</div>
                                     {
-                                        book.authors[0]
+                                        book.authors ? 
+                                        <div className="book-authors">
+                                            {   
+                                                
+                                                book.authors
+                                            }
+                                        </div> : <span>No authors available</span>
                                     }
-                                    </div> : <span>No authors available</span>
-                                }
-                                
-                            </div>
-                        </li>
-                    ))
+                                    
+                                </div>
+                            </li>
+                    )
                 )
+                
                 
             }
 
